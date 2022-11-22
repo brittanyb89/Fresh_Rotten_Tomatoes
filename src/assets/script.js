cfcf2b474fdc6f6c80eb39d15b93db9a7d711224
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-22 12:10:41 -0600

    Added notes into Javascript

---
+++ src/assets/script.js
@@ -0,0 +1,17 @@
+// Activate start button
+
+// Activate dropdown selection
+
+// Show part 1 of quiz only and hide part 2 and three
+
+// Hide part 1 od quiz once quiz is started and first question appears
+
+// Each question is displayed one by one
+
+// Hide part 1 and 2 and display part 3 when last question is answered
+
+// Show scoreboard with top 3 scores
+
+// Activate play again button
+
+// Make replay button restart game (take user back to start screen, part 1)
