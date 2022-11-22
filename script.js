590fadf1fcc79c45c3c04d54a19c439255636723
NhatWin <nhat0933@gmail.com>
2022-11-22 13:33:35 -0600

    Reworked fetch for movie reviews

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
 const review1 = document.querySelector("#choice1");
 const review2 = document.querySelector("#choice2");
 
-let movieReviewTitle1 = {}
+const startButton = document.querySelector("#start-button")
 
-function grabMovieGenre () {
+const selectedGenre = "Action"
+const genreList = ["Romance", "Comedy", "Drama"]
+const randomGenre = genreList[Math.floor(Math.random() *3)]
+
+let movieTitle = {}
+let correctAnswer = {}
+let wrongAnswer = {}
+let genreId = {}
+
+function grabMovieGenre (genre) {
+    // grab genre id list
     fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=d6a051201733ccdafa7109a2dba8cbc6&language=en-US")
     .then(function (movieGenre) {
         return movieGenre.json();
     })
     .then(function (data) {
-        const genreId = data.genres.find((element) => element.name === "Action");
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=d6a051201733ccdafa7109a2dba8cbc6&with_genres=${genreId.id}`)
+        genreId.share = data.genres.find((element) => element.name === genre);
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=d6a051201733ccdafa7109a2dba8cbc6&with_genres=${genreId.share.id}`)
         .then(function (movieList) {
             return movieList.json();
         })
         .then (function (data) {
-
             pullReview (data).then(data=>{
-                console.log('function results',data)
-
+                if (genreId.share.name === selectedGenre) {
+                    correctAnswer = data.results[0].content
+                    console.log(correctAnswer);
+                }   else {
+                    wrongAnswer = data.results[0].content
+                }   
+                displayOptions ()
             })
         })
     })
 }
 
-grabMovieGenre ();
-
 async function pullReview (movieList) {
     const randomMovie = Math.floor(Math.random() * movieList.results.length);
-    movieReviewTitle1 = movieList.results[randomMovie].title
-    console.log(movieReviewTitle1)
+    if (genreId.share.name === selectedGenre) {
+        movieTitle = movieList.results[randomMovie].title
+        console.log(movieTitle)
+    }
+    // grab movie from genre
     const request = await fetch(`https://api.themoviedb.org/3/movie/${movieList.results[randomMovie].id}/reviews?api_key=d6a051201733ccdafa7109a2dba8cbc6&language=en-US&page=1`)
    const jsonResult = await request.json()
    if (jsonResult.results[0]?.content) {
@@ -39,3 +54,13 @@ async function pullReview (movieList) {
    }
 
 }
+
+function displayOptions() {
+
+}
+
+
+
+startButton.addEventListener("click", function(event) {
+    grabMovieGenre (selectedGenre);
+})
