aeae32c909e2c65e44c70725ec94b5cd7712e050
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-24 18:32:18 -0600

    Removed video id to empty string

--- src/assets/movie.js
+++ src/assets/movie.js
@@ -1,3 +1,5 @@
+import script.js from "./src/assets/script.js"
+
 const apiKey = "AIzaSyCmtN6JemTcM9BG7u-ZoTgEdKS1-m8ngOk";
 const getRequest = "https://www.googleapis.com/youtube/v3/videos";
 
@@ -14,7 +16,7 @@ function onYouTubeIframeAPIReady() {
   player = new onYouTubeIframeAPIReady.Player("player", {
     height: "390",
     width: "640",
-    videoId: "M7lc1UVf-VE",
+    videoId: "",
     playerVars: {
       playsinline: 1,
     },
@@ -23,8 +25,21 @@ function onYouTubeIframeAPIReady() {
       onStateChange: onPlayerStateChange,
     },
   });
-}
+};
 // API will call this function when the video player is ready
 function onPlayerReady(event) {
   event.target.playVideo();
-}
+};
+
+// API calls function when player state changes; the player should play for six seconds and stop.
+let done = false;
+function onPlayerStateChange(event) {
+    if (event.data == onYouTubeIframeAPIReady.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+},
+function stopVideo() {
+    player.stopVideo();
+};
+
