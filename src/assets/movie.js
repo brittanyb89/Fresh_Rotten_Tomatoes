fef7a25abc0179a40a9e5c931df5b745a2f793d2
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-26 10:18:45 -0600

    Changed div id for player

--- src/assets/movie.js
+++ src/assets/movie.js
@@ -2,8 +2,13 @@ import script.js from "./src/assets/script.js"
 
 const apiKey = "AIzaSyCmtN6JemTcM9BG7u-ZoTgEdKS1-m8ngOk";
 const getRequest = "https://www.googleapis.com/youtube/v3/videos";
+// https://www.youtube.com/embed?listType=playlist&list=PL${original_title}
 
 let tag = document.createElement("script");
+tag.id = 'iframe-demo';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // Loads the IFrame Player API Javascript code using DOM modification to download the API code to ensure that code is retrieved asynchronously.
 tag.src = "https://www.youtube.com/player_api";
@@ -16,7 +21,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player("ytplayer", {
     height: "390",
     width: "640",
-    videoId: "genres",
+    videoId: "original_title",
     playerVars: {
       playsinline: 1,
     },
@@ -28,6 +33,7 @@ function onYouTubeIframeAPIReady() {
 };
 // API will call this function when the video player is ready
 function onPlayerReady(event) {
+    let cc_load_policy = 1;
   event.target.playVideo();
 };
 
@@ -43,3 +49,13 @@ function stopVideo() {
     player.stopVideo();
 };
 
+// YouTube API call for movie clips
+cueVideoById();
+
+aync function cueVideoById() {
+    const request = await fetch(
+
+    )
+}
+player.cueVideoById({videoId: 'original_title', startSeconds: 3
+});
